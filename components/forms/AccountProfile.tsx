5ba62e0a9e623a984b6f7f359a1da5cca64b24ee
unknown <kmrinandita@gmail.com>
2023-09-23 00:47:48 +0530

    on progress

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -14,6 +14,7 @@ import {
   FormField,
   FormItem,
   FormLabel,
+  FormMessage
   
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
@@ -146,7 +147,8 @@ Props) => {
                     className='account-form_image-input'
                     onChange={(e) => handleImage(e, field.onChange)}
                 />
-              </FormControl>              
+              </FormControl>    
+              <FormMessage />
             </FormItem>
           )}
         />
@@ -165,7 +167,8 @@ Props) => {
                     className='account-form_input no-focus'
                     {...field}
                 />
-              </FormControl>              
+              </FormControl>     
+              <FormMessage />         
             </FormItem>
           )}
         />
@@ -184,7 +187,8 @@ Props) => {
                     className='account-form_input no-focus'
                     {...field}
                 />
-              </FormControl>              
+              </FormControl>    
+              <FormMessage />          
             </FormItem>
           )}
         />
@@ -203,7 +207,8 @@ Props) => {
                     className='account-form_input no-focus'
                     {...field}
                 />
-              </FormControl>              
+              </FormControl>   
+              <FormMessage />           
             </FormItem>
           )}
         />
